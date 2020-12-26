d4ba0ca5fc9014ad31c22a78d1b52370010d9cf1
Artem Motuzniy <developer.artemmotuzniy@gmail.com>
2020-12-26 14:37:58 +0200

    fix

--- app/src/main/java/com/developerartemmotuznyi/sdhtest/presentation/medicine/PagingMedicineSource.kt
+++ app/src/main/java/com/developerartemmotuznyi/sdhtest/presentation/medicine/PagingMedicineSource.kt
@@ -2,7 +2,7 @@ package com.developerartemmotuznyi.sdhtest.presentation.medicine
 
 import androidx.paging.PagingSource
 import com.developerartemmotuznyi.sdhtest.core.model.handleWithResult
-import com.developerartemmotuznyi.sdhtest.local.model.MedicineEntity
+import com.developerartemmotuznyi.sdhtest.domain.model.Medicine
 import com.developerartemmotuznyi.sdhtest.domain.model.PagingResult
 import com.developerartemmotuznyi.sdhtest.domain.usecase.LoadMedicineUseCase
 import com.developerartemmotuznyi.sdhtest.domain.usecase.SearchMedicineUseCase
@@ -11,9 +11,9 @@ class PagingMedicineSource(
     private val query: String,
     private val loadMedicineUseCase: LoadMedicineUseCase,
     private val searchMedicineUseCase: SearchMedicineUseCase
-) : PagingSource<Int, MedicineEntity>() {
+) : PagingSource<Int, Medicine>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MedicineEntity> = try {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Medicine> = try {
         val nextPage = getNextKey(params.key)
         val result = loadData(nextPage)
         result.handleWithResult(::handleSuccess, ::handleError)
@@ -21,10 +21,10 @@ class PagingMedicineSource(
         LoadResult.Error(e)
     }
 
-    private fun handleError(throwable: Throwable): LoadResult<Int, MedicineEntity> =
+    private fun handleError(throwable: Throwable): LoadResult<Int, Medicine> =
         LoadResult.Error(throwable)
 
-    private fun handleSuccess(pagingResult: PagingResult): LoadResult<Int, MedicineEntity> =
+    private fun handleSuccess(pagingResult: PagingResult): LoadResult<Int, Medicine> =
         LoadResult.Page(
             pagingResult.result,
             if (pagingResult.previousPageIndex == -1) null else pagingResult.previousPageIndex,
